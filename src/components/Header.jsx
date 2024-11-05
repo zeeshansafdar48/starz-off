1ee4f4528901d011839a4399df45442e896f5cc8
Zeeshan Safdar <zeeshan7826@gmail.com>
2024-11-05 22:50:34 +0500

    working on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 import { paths } from "../pages/routes";
 import logo from "../assets/images/logo.svg";
+import { useState } from "react";
 
 function Header() {
   return (
@@ -12,7 +13,9 @@ function Header() {
           <img src={logo} alt="Logo" height={100} width={180} />
         </NavItem>
         <NavItem to={paths.home}>Home</NavItem>
+        <NavItem to={paths.home}>Movies</NavItem>
         <NavItem to={paths.popularPeople}>People</NavItem>
+        <NavList>Test</NavList>
       </ul>
     </div>
   );
@@ -24,6 +27,36 @@ function NavItem({ to, children }) {
       <Link to={to}>{children}</Link>
     </li>
   );
+}
+
+function NavList({ children }) {
+  const [showList, setShowList] = useState(true);
+
+  function handleShowList(isShowList) {
+    setShowList(isShowList);
+  }
+
+  return (
+    <li
+      className="hover:cursor-pointer"
+      onMouseEnter={() => handleShowList(true)}
+      onMouseLeave={() => handleShowList(true)}
+    >
+      {children}
+      {showList && (
+        <div className="bg-secondary-500 absolute flex gap-4 flex-col rounded-md">
+          <NavListItem>Popular</NavListItem>
+          <NavListItem>Now Playing</NavListItem>
+          <NavListItem>Upcoming</NavListItem>
+          <NavListItem>Top Rated</NavListItem>
+        </div>
+      )}
+    </li>
+  );
+}
+
+function NavListItem({ children }) {
+  return <div className="hover:bg-gray-300 mx-3">{children}</div>;
 }
 
 export default Header;
