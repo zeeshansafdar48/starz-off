4346797429e1f973a144d11f876740e1157de025
Zeeshan Safdar <zeeshan7826@gmail.com>
2024-11-06 21:44:56 +0500

    handle load more data on movies page

--- src/pages/Movies/Popular.jsx
+++ src/pages/Movies/Popular.jsx
@@ -8,26 +8,53 @@ import { MOVIES_IMAGE_BASE_URL } from "../../constants";
 import { formatDate } from "../../utils/formatDate";
 import CircularProgressBar from "./CircularProgressBar";
 
+let isDataFetchedOnInitialLoad = false;
+
 function PopularMovies() {
   const [isLoading, setIsLoading] = useState(false);
   const [movies, setMovies] = useState({
-    page: 1,
+    page: 0,
     results: [],
     total_pages: 1,
     total_results: 0
   });
-  console.log("==> ~ Popular ~ movies:", movies);
 
   useEffect(() => {
     async function fetchPeople() {
       setIsLoading(true);
-      const data = await fetchPopularMovies(movies?.page);
-      setMovies(data);
-      setIsLoading(false);
+      try {
+        const data = await fetchPopularMovies(movies?.page || 1);
+
+        if (data.results) {
+          setMovies((prevState) => {
+            return {
+              page: data.page,
+              results: [...prevState.results, ...data.results],
+              total_pages: data.total_pages,
+              total_results: data.total_results
+            };
+          });
+          setIsLoading(false);
+          isDataFetchedOnInitialLoad = true;
+        }
+      } catch (error) {
+        console.log("==> ~ fetchPeople ~ error:", error);
+      }
     }
+
+    if (isDataFetchedOnInitialLoad && movies?.page === 1) return;
 
     fetchPeople();
   }, [movies?.page]);
+
+  async function handleLoadMore() {
+    setMovies((prevState) => {
+      return {
+        ...prevState,
+        page: prevState.page + 1
+      };
+    });
+  }
 
   return (
     <div>
@@ -38,6 +65,11 @@ function PopularMovies() {
           return <MovieItem key={movie?.id} movie={movie} />;
         })}
       </ul>
+      <div className="flex justify-center items-center my-10">
+        {movies?.page < movies.total_pages && (
+          <LoadMore onClick={handleLoadMore} isLoading={isLoading} />
+        )}
+      </div>
     </div>
   );
 }
@@ -51,12 +83,6 @@ function MovieItem({ movie }) {
         alt={movie?.title}
         className="w-full rounded-t-lg"
       />
-      {/* <div className="w-full bg-gray-300 h-1">
-        <div
-          className={`h-1 bg-gradient-to-r from-tertiary-500 to-secondary-500 text-xs font-sm text-blue-100 text-center leading-none`}
-          style={{ width: `${ratings}%` }}
-        ></div>
-      </div> */}
 
       <div className="pl-3 pb-4 mt-2 relative">
         <div className="absolute -top-8">
@@ -66,6 +92,18 @@ function MovieItem({ movie }) {
         <p className="text-sm text-black/60">{formatDate(movie?.release_date)}</p>
       </div>
     </li>
+  );
+}
+
+function LoadMore({ onClick, isLoading }) {
+  return (
+    <button
+      onClick={onClick}
+      className="text-lg font-semibold text-white px-8 py-4 rounded-lg transition disabled:cursor-not-allowed disabled:bg-gray-300 disabled:bg-gradient-to-r disabled:from-secondary-200 disabled:to-tertiary-200 bg-gradient-to-r from-secondary-500 to-tertiary-500 "
+      disabled={isLoading}
+    >
+      Load More
+    </button>
   );
 }
 
